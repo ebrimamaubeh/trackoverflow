ba9b13677a2c186936ae2efec2e2551f63901b7a
ejallow <ebrimamaubeh7@gmail.com>
2021-09-30 22:24:02 +0200

    deleted files not needeed

--- posts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from models import Question
-
-def index(requests):
-    # this function should return a list of all post. 
-    questions = Question.objects.all()
-    return render(requests, contex=questions)
